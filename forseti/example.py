3b22deeefc866d37da9aeb33e6adc8208890ba77
bcwein <bcwein343@gmail.com>
2022-01-26 12:40:05 +0100

    test module added.

---
+++ forseti/example.py
@@ -0,0 +1,13 @@
+"""Test Function."""
+
+
+def add_one(number):
+    """add_one
+
+    Args:
+        number (number): Number that we want to increment with 1.
+
+    Returns:
+        number: number + 1
+    """
+    return number + 1
