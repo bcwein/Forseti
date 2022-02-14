3ed21be2afe30bcfb55c9a1fb86e7a0179de1397
bcwein <bcwein343@gmail.com>
2022-02-14 11:15:44 +0100

    Notebook for running code.

--- notebooks/baseline.py
+++ notebooks/baseline.py
@@ -7,10 +7,15 @@ module_path = os.path.abspath(os.path.join(".."))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-from forseti.datproc import translate_categorical
+from forseti.datproc import translate_categorical, extract_sensitive
 
-# %%
+# %% Translate Categorical Attributes
 df = pd.read_csv('data/adult.csv')
 tmp, codes = translate_categorical(df)
+
+# %% Extract sensitive attributes
+sensattr = ['gender', 'race']
+sensitive, features = extract_sensitive(tmp, sensattr)
+
 # %%
-codes
+codes['relationship']
