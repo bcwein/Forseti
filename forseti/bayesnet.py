d6b46f808ed45935b2f80e510249f38c1675edb2
bcwein <bcwein343@gmail.com>
2022-03-08 16:37:41 +0100

    Created latentLabelClassifier class.

--- forseti/bayesnet.py
+++ forseti/bayesnet.py
@@ -3,44 +3,102 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.estimators import HillClimbSearch
 from pgmpy.estimators import ExpectationMaximization as EM
-from pgmpy.base import DAG
 from forseti.datproc import translate_categorical
+import pickle
 
 
-def latent_label_classifier(df, sensitives, label, atol=0.01):
-    tmp, codes = translate_categorical(df.copy(deep=True))
-    tmp = tmp.drop(label, axis=1)
+class latentLabelClassifier:
+    """Latent Fair Label Bayesian Network Classifier."""
 
-    sensitive = ['gender', 'race']
-    blacklist = []
-    for col in tmp.columns:
-        blacklist.append((col, sensitive[0]))
-        blacklist.append((col, sensitive[1]))
+    def __init__(self, df, sensitives, label, atol=0.01, classes=2):
+        self.df = df
+        self.sensitives = sensitives
+        self.label = label
+        self.atol = atol
+        self.classes = classes
+        self.model = BayesianNetwork()
+        self.codes = None
 
-    c = HillClimbSearch(tmp)
-    model = c.estimate(black_list=blacklist)
+    def fit(self):
+        """
+        Constructs a bayesian network with a fair latent variable.
 
-    for sens in sensitives:
-        model.add_edge(sens, label)
+        Structure Learning: Hill Climb Search
+        Parameter Learning: Expectation Maximation (EM)
+        """
+        tmp, codes = translate_categorical(self.df.copy(deep=True))
+        tmp = tmp.drop(self.label, axis=1)
 
-    tmp, codes = translate_categorical(df.copy(deep=True))
+        blacklist = []
+        for col in tmp.columns:
+            blacklist.append((col, self.sensitives[0]))
+            blacklist.append((col, self.sensitives[1]))
 
-    # Connect latent to features
-    for col in tmp.columns:
-        if col in sensitives:
-            continue
-        else:
-            model.add_edge('fair', col)
+        c = HillClimbSearch(tmp)
+        model = c.estimate(black_list=blacklist)
 
-    # Connect latent to label
-    model.add_edge('fair', label)
+        for sens in self.sensitives:
+            model.add_edge(sens, self.label)
 
-    fair_model = BayesianNetwork(list(model.edges()), latents={'fair'})
+        tmp, codes = translate_categorical(self.df.copy(deep=True))
 
-    estimator = EM(fair_model, tmp)
-    cpds = estimator.get_parameters(latent_card={'fair': 2}, atol=atol)
+        # Connect latent to features
+        for col in tmp.columns:
+            if col in self.sensitives:
+                continue
+            else:
+                model.add_edge("fair", col)
 
-    for cpd in cpds:
-        fair_model.add_cpds(cpd)
+        # Connect latent to label
+        model.add_edge("fair", self.label)
 
-    return fair_model, codes
+        fair_model = BayesianNetwork(list(model.edges()), latents={"fair"})
+
+        estimator = EM(fair_model, tmp)
+        cpds = estimator.get_parameters(
+            latent_card={"fair": self.classes}, atol=self.atol
+        )
+
+        for cpd in cpds:
+            fair_model.add_cpds(cpd)
+
+        self.model = fair_model
+        self.codes = codes
+
+    def predict_probability(self, test):
+        """Predict and return probabilities.
+
+        Args:
+            test (Dataframe): Test Dataset
+
+        Return:
+            dataframe: Dataframe with predictions for unobserved variables.
+        """
+        return self.model.predict_probability(test)
+
+    def predict(self, test):
+        """Predict and return prediction labels.
+
+        Args:
+            test (Dataframe): Test Dataset
+
+        Return:
+            dataframe: Dataframe with predictions for unobserved variables.
+        """
+        return self.model.predict(test)
+
+    def load(self, file):
+        """Load trained model from file using pickle.
+
+        Args:
+            file (string): file path of trained model.
+        """
+        self.model = pickle.load(open(file, 'rb'))
+
+    def save(self, file):
+        """Save trained model to file using pickle.
+
+        Args:
+            file (filetype): _description_
+        """
+        pickle.dump(self.model, open(file, 'wb'))
