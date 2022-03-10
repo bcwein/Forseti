26bd31179dca0a255a44d1b83b8d6b100a1769c5
bcwein <bcwein343@gmail.com>
2022-03-10 14:04:45 +0100

    test for parity score added.

---
+++ tests/test_metrics.py
@@ -0,0 +1,8 @@
+from forseti.fairness import parity_score, fairness_report
+
+
+def test_parity_score():
+    probs = [0.5, 0.5]
+    assert parity_score(probs) == 1
+    probs = [1, 0]
+    assert parity_score(probs) == 0
